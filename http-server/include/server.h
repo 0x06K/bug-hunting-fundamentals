2c0b1ded3c9cd2e7393117d5ef4173c04f004686
archlinuxuser <161124933+0x06K@users.noreply.github.com>
2025-08-30 04:25:49 +0000

    server.h

---
+++ http-server/include/server.h
@@ -0,0 +1,28 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define DEFAULT_PORT 8080
+#define BACKLOG 10
+#define MAX_CLIENTS 100
+
+typedef struct {
+    int socket_fd;
+    int port;
+    struct sockaddr_in address;
+    int running;
+} server_t;
+
+// Server lifecycle functions
+int server_init(server_t *server, int port);
+int server_start(server_t *server);
+void server_run(server_t *server);
+void server_stop(server_t *server);
+void server_cleanup(server_t *server);
+
+// Client handling
+void handle_client(int client_fd);
+
+#endif // SERVER_H
